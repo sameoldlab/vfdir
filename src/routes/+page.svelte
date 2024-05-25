5a97d257dc085b133f8226267b205ac062098f4a
ibro <sameoldlab@proton.me>
2024-05-25 20:35:01 +0100

    install sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import GridView from "$lib/GridView.svelte";
+  import { getChannels } from '$lib/stores/arena-apiv2.svelte.js'
+
+  const channels = getChannels()
+</script>
+
+<main>
+  <GridView content={channels.list} />
+</main>
