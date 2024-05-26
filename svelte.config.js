2aeea55dc5cbce0bd4550a6ce71b09fa9cefd809
ibro <sameoldlab@proton.me>
2024-05-26 18:02:04 +0100

    switch to (semi-)official sqlite esm package

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-auto'
+
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
