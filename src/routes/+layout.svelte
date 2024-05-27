46e37178d2bfdf4f50eedfe447388659b63521ae
ibro <sameoldlab@proton.me>
2024-05-27 17:06:20 +0100

    initialize database

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
-	import '../app.css'
-	import Header from '$lib/components/header.svelte'
+  import '../app.css'
+  import Header from '$lib/components/header.svelte'
+  import { initSqlite } from '$lib/store/initSqlite'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+  	initSqlite()
+  });
+
 </script>
-<Header/>
-<slot/>
+
+<Header />
+<slot />
