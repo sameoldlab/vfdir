435f7435f7f0ff46d6f126fb14cb756e1eaa253d
ibro <sameoldlab@proton.me>
2024-05-28 15:20:16 +0100

    initialize database and save promiser as svelte rune

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,30 @@
 <script>
   import '../app.css'
   import Header from '$lib/components/header.svelte'
-  import { initSqlite } from '$lib/store/initSqlite'
+  import { initSqlite, getPromiser } from '$lib/store/initSqlite.svelte'
   import { onMount } from 'svelte'
 
-  onMount(() => {
-  	initSqlite()
+  onMount(async () => {
+		let file = 'vfdir'
+		const SQL = await getPromiser()
+  	await initSqlite(file)
+
+		let {dbId} = await SQL.promiser('open', {filename: `file:${file}.sqlite3?vfs=opfs`})
+
+		console.log('', 'Query data with exec()')
+    SQL.promiser('exec', {
+      dbId,
+      sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
+      callback: (result) => {
+				console.log(result.type)
+        if (!result.row) {
+          return
+        }
+        console.log('', result.row)
+      },
+    })
+		
+	SQL.promiser('close', { dbId: dbId })
   });
 
 </script>
