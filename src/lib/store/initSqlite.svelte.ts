820a50964cade6e185569b3bb2a69a0568657f0e
ibro <sameoldlab@proton.me>
2024-05-28 15:20:16 +0100

    initialize database and save promiser as svelte rune

--- src/lib/store/initSqlite.svelte.ts
+++ src/lib/store/initSqlite.svelte.ts
@@ -1,31 +1,58 @@
 export async function getPromiser() {
 	const { sqlite3Worker1Promiser } = await import(`@sqlite.org/sqlite-wasm`)
 
-	const log = console.log
-	const error = console.error
-	const promiser = await sqlite3Worker1Promiser.v2()
+	let promiser = $state(
+		await sqlite3Worker1Promiser.v2({
+			debug(...args) {
+				// log(args)
+			},
+		})
+	)
+
+	return {
+		get promiser() {
+			return promiser
+		},
+	}
+}
+
+export async function initSqlite(file: string) {
+	const SQL = await getPromiser()
 
 	try {
-		log('Loading and initializing SQLite3 module...')
+		console.log('Loading and initializing SQLite3 module...')
 
-		log('Done initializing. Running demo...')
+		let configResponse = await SQL.promiser('config-get', {})
+		console.info('Running SQLite3 version', configResponse.result.version.libVersion)
 
-		const configResponse = await promiser('config-get', {})
-		log('Running SQLite3 version', configResponse)
-		const openResponse = await promiser('open', {
-			filename: 'file:mydb.sqlite3?vfs=opfs',
+		const openResponse = await SQL.promiser('open', {
+			filename: `file:${file}.sqlite3?vfs=opfs`,
 		})
-		const { dbId } = openResponse
-		log(
-			'OPFS is available, created persisted database at',
-			openResponse.result.filename.replace(/^file:(.*?)\?vfs=opfs$/, '$1')
-		)
-		
-		
+		const {dbId} = openResponse
+		if (openResponse.type !== 'error') {
+			console.info(
+				'OPFS is available, created persisted database at',
+				openResponse.result.filename.replace(/^file:(.*?)\?vfs=opfs$/, '$1')
+			)
+		}
+
+		await SQL.promiser('exec', { dbId, sql: 'CREATE TABLE IF NOT EXISTS t(a,b)' })
+    console.log('', 'Creating a table...')
+
+    console.log('', 'Insert some data using exec()...')
+    for (let i = 20; i <= 25; ++i) {
+      await SQL.promiser<'exec'>('exec', {
+        dbId,
+        sql: 'INSERT INTO t(a,b) VALUES (?,?)',
+        bind: [i, i * 2],
+      })
+    }
+
 	} catch (err) {
 		if (!(err instanceof Error)) {
 			err = new Error(err.result.message)
 		}
-		error(err.name, err.message)
+		console.error(err.name, err.message)
 	}
+
 }
