7cab3b1fce895a0ca22fdcb4b5b565925e3837d5
ibro <sameoldlab@proton.me>
2024-05-29 18:21:22 +0100

    move promiser to global context

---
+++ src/lib/store/promiser.ts
@@ -0,0 +1,27 @@
+import { getContext, setContext } from 'svelte'
+import { type Writable, writable } from 'svelte/store'
+import type { Promiser } from '@sqlite.org/sqlite-wasm'
+import { sqlite3Worker1Promiser } from '@sqlite.org/sqlite-wasm'
+
+export async function createPromiser() {
+	let promiser = await sqlite3Worker1Promiser
+		.v2({
+			debug(...args) {
+				// console.log(args)
+			},
+		})
+	return promiser
+}
+
+export const promiser = createPromiser()
+
+export function setPromiser(promiser: Promise<Promiser>) {
+	const value = writable(promiser)
+
+	setContext('promiser', value)
+}
+
+export function getPromiser() {
+	const promiser = getContext<Writable<Promiser>>('promiser')
+	return promiser
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,24 @@
 <script>
   import '../app.css'
   import Header from '$lib/components/header.svelte'
-  import { initSqlite, getPromiser } from '$lib/store/initSqlite.svelte'
+  import { initSqlite } from '$lib/store/initSqlite.svelte'
   import { onMount } from 'svelte'
+	import { getPromiser, promiser, setPromiser } from '$lib/store/promiser'
+	let { children } = $props();
 
-  onMount(async () => {
-		let file = 'vfdir'
-		const SQL = await getPromiser()
-  	await initSqlite(file)
-
-		let {dbId} = await SQL.promiser('open', {filename: `file:${file}.sqlite3?vfs=opfs`})
-
-		console.log('', 'Query data with exec()')
-    SQL.promiser('exec', {
-      dbId,
-      sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
-      callback: (result) => {
-				console.log(result.type)
-        if (!result.row) {
-          return
-        }
-        console.log('', result.row)
-      },
-    })
-		
-	SQL.promiser('close', { dbId: dbId })
-  });
-
+	setPromiser(promiser)
+	let res = getPromiser()
+	let resolved = $state(false)
+	
+	onMount(async () => {
+		$res = await Promise.resolve($res)
+		initSqlite($res)
+		resolved = true
+	})
 </script>
 
 <Header />
-<slot />
+
+{#if resolved }
+	{@render children()}
+{/if}
