d5c4c4f7959d4628b4fa5d7f523fa18f147ef247
ibro <sameoldlab@proton.me>
2024-05-31 11:15:09 +0100

    elegance, at long last

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,15 @@
 <script>
   import '../app.css'
   import Header from '$lib/components/header.svelte'
-  import { initSqlite } from '$lib/store/initSqlite.svelte'
-  import { onMount } from 'svelte'
-	import { getPromiser, promiser, setPromiser } from '$lib/store/promiser'
+	import { db } from '$lib/store/initSqlite.svelte'
 	let { children } = $props();
-
-	setPromiser(promiser)
-	let res = getPromiser()
-	let resolved = $state(false)
 	
-	onMount(async () => {
-		$res = await Promise.resolve($res)
-		await initSqlite($res)
-		resolved = true
-	})
 </script>
 
 <Header />
 
-{#if resolved }
+{#if !db.fulfilled }
+	setting up sqlite
+{:else}
 	{@render children()}
 {/if}
