758cedba6c65dfdf3474cd02f6c1c91a8274573d
ibro <sameoldlab@proton.me>
2024-07-04 21:03:42 +0100

    draggable panes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
   import '../app.css'
   import Header from '$lib/components/header.svelte'
 	import { db } from '$lib/store/initSqlite.svelte'
+	// import { bootstrap } from '$lib/store/data.svelte'
 	let { children } = $props();
-	
 </script>
 
 <Header />
 
-{#if !db.fulfilled }
+{#if db.fulfilled === false }
 	setting up sqlite
 {:else}
 	{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,134 @@
 <script lang="ts">
-	import GridView from '$lib/components/GridView.svelte'
-	import { getChannels } from '$lib/store/data.svelte.js'
-	import { getChannels as getArenaChannels } from '$lib/store/arena-apiv2.svelte'
-	import { db } from '$lib/store/initSqlite.svelte'
-	
-	let channels = getChannels(db.promiser)
-	
-	console.log('fetching "channels"')
-	getArenaChannels().then(async res => {
-		console.log(res)
-		await channels.push(res.channels)
-		console.log('Filled', channels.list)
-	})
-	
+	// import GridView from '$lib/components/GridView.svelte'
+	import { channels } from '$lib/store/data.svelte.js'
+	import type { Action } from 'svelte/action'
+	let leftWidth: number
+	const resizer: Action<HTMLDivElement> = el => {
+		const leftPane = el.previousElementSibling as HTMLDivElement
+		const rightPane = el.nextElementSibling as HTMLDivElement
+		// let directionX = 0
+
+		const doc_mouseMove = (e: MouseEvent) => {
+			leftPane.style.width = `${leftWidth + e.movementX}px`
+			document.body.style.cursor = 'col-resize'
+			document.body.style.userSelect = 'none'
+			document.body.style.pointerEvents = 'none'
+			// directionY = e.movementY
+		}
+		const doc_mouseUp = () => {
+			document.body.style.removeProperty('cursor')
+			document.body.style.removeProperty('user-select')
+			document.body.style.removeProperty('pointer-events')
+
+			document.removeEventListener('mousemove', doc_mouseMove)
+			document.removeEventListener('mouseup', doc_mouseUp)
+		}
+
+		const el_mouseDown = () => {
+			document.addEventListener('mousemove', doc_mouseMove)
+			document.addEventListener('mouseup', doc_mouseUp)
+		}
+
+		el.addEventListener('mousedown', el_mouseDown)
+
+		return {
+			destroy() {
+				el.removeEventListener('mousedown', el_mouseDown)
+				document.removeEventListener('mousemove', doc_mouseMove)
+				document.removeEventListener('mouseup', doc_mouseUp)
+			},
+		}
+	}
 </script>
 
 <main>
-	{#if channels.list}
-		<GridView content={channels} />
-	{/if}
+	<div class="pane left" bind:clientWidth={leftWidth}>
+		<a href="/df/adsaf" class="item">link</a>
+		<a href="/gf/adsdsafdsf" class="item">link</a>
+		<a href="/rtf/fsdsaf" class="item">link</a>
+		<a href="/dhf/adsdsf" class="item">link</a>
+		<a href="/dh/adsaf" class="item">link</a>
+		<a href="/nf/adfsaf" class="item">link</a>
+		<a href="/erwf/adsdaf" class="item">link</a>
+		{#each channels.list as { title, slug }}
+			{title}
+			<!-- <GridView content={channels} /> -->
+		{:else}
+			<div class="item">empty</div>
+		{/each}
+	</div>
+	<div class="handle" draggable use:resizer>
+		<div></div>
+	</div>
+	<div class="pane right">
+		<a href="/df/adsaf" class="item"> link</a>
+		<a href="/gf/adsdsafdsf" class="item"> link</a>
+		<a href="/rtf/fsdsaf" class="item"> link</a>
+		<a href="/dhf/adsdsf" class="item"> link</a>
+		<a href="/dh/adsaf" class="item"> link</a>
+		<a href="/nf/adfsaf" class="item"> link</a>
+		<a href="/erwf/adsdaf" class="item"> link</a>
+	</div>
+	<!-- <div class="handle"></div> -->
 </main>
+
+<style>
+	main {
+		flex-grow: 1;
+
+		display: flex;
+	}
+
+	.handle {
+		width: 1rem;
+		/* opacity: 0; */
+		outline: 4px solid transparent;
+		display: flex;
+		justify-content: center;
+		transition: background-color 100ms 80ms ease-out;
+		
+		&:hover {
+			animation: delayed-resize 0ms 150ms ease-out forwards;
+			background: oklch(0.24 0 89.88);
+			/* outline: 4px solid oklch(.3 0 0); */
+			/* delay 200ms then transition cursor to resize */
+			/* width: 4px; */
+			opacity: 1;
+		}
+		& div {
+			width: 1px;
+			height: 100%;
+			background: oklch(0.32 0 89.88);
+			opacity: 1;
+		}
+	}
+
+	@keyframes delayed-resize {
+		from {
+			pointer-events: none;
+		}
+		to {
+			pointer-events: auto;
+			cursor: col-resize;
+		}
+	}
+	.pane {
+		display: flex;
+		flex-direction: column;
+		width: 20ch;
+		/* border-inline-end: 1px solid hsl(0 0% 24%); */
+		/* padding: .5em; */
+		.item {
+			/* border-block: 1px solid ; */
+			padding: 0.25em 0.75em;
+			&:hover,
+			&:focus {
+				background: red;
+			}
+		}
+	}
+	.pane.right {
+		/* background: brown; */
+		flex: 1;
+	}
+</style>
