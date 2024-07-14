8b93a71c268e55a58ea4b6295b18d187dfff5551
ibro <sameoldlab@proton.me>
2024-07-14 22:40:37 +0100

    cache data n sql

--- src/lib/store/data.svelte.ts
+++ src/lib/store/data.svelte.ts
@@ -11,59 +11,59 @@
 
 import type { Promiser } from '@sqlite.org/sqlite-wasm'
 import { filename } from './initSqlite.svelte'
+type ChannelStatus = "local" | "closed" | "public" | "open"
+export type Channel = {
+	slug: string
+	title: string
+	created_at: string
+	status: ChannelStatus
+	author_slug: string
+	flags: string[]
+}
+class Channels {
+	list = $state<Channel[]>([])
 
-export function getChannels(promiser: Promiser) {
-	let list = $state<
-		{
-			slug: string
-			title: string
-			created_at: string
-			status: string
-		}[]
-	>([])
-	const update = async () => {
-		const { dbId } = await promiser('open', { filename })
-		console.log(`opening ${dbId} for update`)
+	async pull(promiser: Promiser, dbId: string | undefined) {
+		dbId = dbId ?? (await promiser('open', { filename })).dbId
+		console.log(`updating with ${dbId}`)
 
 		await promiser('exec', {
 			dbId,
-			sql: 'SELECT slug,title,created_at,status FROM Channels LIMIT 50',
+			sql: 'SELECT slug,title,created_at,status,author_slug,flags FROM Channels LIMIT 50',
 			callback: result => {
 				if (!result.row) return
 				const { row } = result
-				if (list.find(i => i.slug === row[0]) !== undefined) return
+				if (this.list.find(i => i.slug === row[0]) !== undefined) return
 				const item = {
 					slug: row[0],
 					title: row[1],
 					created_at: row[2],
 					status: row[3],
+					author_slug: row[4],
+					flags: row[5]
 				}
 				console.log(item)
 				console.log('pulled item from db')
 
-				list.push(item)
-				console.log('saved item from to list')
-				console.log(list)
+				this.list.push(item)
+				console.log('saved item from to this.list')
+				console.log(this.list)
 			},
 		})
 		await promiser('close', { dbId })
-		console.log(list)
+		console.log(this.list)
 	}
-	update()
 
-	/** Checks for duplicates then adds each element to database*/
-	const push = async (vals: typeof list) => {
+	async push(promiser: Promiser, channels: typeof this.list) {
+		console.log('promiser is ', promiser)
+		
 		const { dbId } = await promiser('open', { filename })
 		console.log(`opening ${dbId} for push`)
 
-		vals.forEach(v => {
-			console.log(
-				'wtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtfwtf'
-			)
+		channels.forEach(v => {
 			console.log(v)
-
 			if (
-				list.find(i => {
+				this.list.find(i => {
 					console.log(i)
 					return i?.slug === v.slug
 				}) !== undefined
@@ -73,20 +73,29 @@ export function getChannels(promiser: Promiser) {
 			promiser('exec', {
 				dbId,
 				sql: /* sql */ `
-				INSERT INTO Channels(title,slug,created_at,status) VALUES (?,?,?,?)`,
-				bind: [v.title, v.slug, v.created_at, v.status],
+					INSERT INTO Channels(slug,title,created_at,status,author_id,flags) VALUES(?,?,?,?,?,?);
+				`,
+				bind: [v.slug, v.title, v.created_at, v.status, v.author_slug, v.flags],
 			})
 		})
-		promiser('close', { dbId })
-		console.log(list)
-		await update()
-	}
 
-	return {
-		get list() {
-			return list
-		},
-		update,
-		push,
+		this.pull(promiser, dbId)
+		promiser('close', { dbId })
+		// console.log(this.list)
 	}
+}
+
+export const channels = new Channels()
+
+function populateChannels() {
+	/* 
+		1. fetch 
+		2. save channels 
+		3. save any users found 
+		4. save any blocks found 
+	
+	*/
+}
+function getBlocks() {
+	
 }
