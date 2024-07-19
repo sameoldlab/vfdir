19bc4864d61e0cbc40df353933b0e29e2e460704
ibro <sameoldlab@proton.me>
2024-07-19 13:18:29 +0100

    add raindrop api

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ARENA_KEY: string
+  readonly VITE_RAINDROP_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
