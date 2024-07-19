a0fa696cb0ba8ddf8420d7b1fbecdb830eae3e6f
ibro <sameoldlab@proton.me>
2024-07-19 13:21:59 +0100

    mv tinyb to sqlite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import '../app.css'
   import Header from '$lib/components/header.svelte'
-	import { db } from '$lib/store/tinyb.svelte'
+	import { db } from '$lib/store/sqlite.svelte'
 	import { channels } from '$lib/store/data.svelte'
 	let { children } = $props();
 
