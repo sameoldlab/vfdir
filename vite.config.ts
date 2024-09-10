ce99f0b2756e85c219d0c3f6ba3a615b302e5a2c
ibro <sameoldlab@proton.me>
2024-09-10 08:55:36 +0100

    add `proxy: {}`
    fixes sveltekit issues with cetificates (on Node 20?)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
+		proxy: {}
 	plugins: [
 		mkcert({
 			mkcertPath: '/usr/bin/mkcert',
