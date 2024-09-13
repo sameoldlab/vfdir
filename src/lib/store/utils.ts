6b2c58c2e0fb1e56de0c712f73e0f5bc661ac647
ibro <sameoldlab@proton.me>
2024-09-13 16:09:38 +0100

    convert classes to insert functions

--- src/lib/store/utils.ts
+++ src/lib/store/utils.ts
@@ -29,126 +29,63 @@ export type Channel = {
 	blocks: Block[];
 };
 
-const keys = Object.freeze({
-	Channels: "slug",
-	Blocks: "id",
-});
-
-async function pull<V extends object, K extends keyof V>({
-	db,
-	row,
-	list,
-	key,
-	table,
-}: {
-	db: DB;
-	row: bigint | undefined;
-	list: Map<V[K], V>;
-	key: K;
-	table: string;
-}) {
-	const query = await db.execO<V>(
-		`SELECT * FROM ${table}${row === undefined ? "" : ` WHERE rowId = ${row}`};`,
+/**
+	Insert an array off items into the given database.
+	Expects the table to already exist
+	@param {DB} db - DB Connection. Recommended to run inside a transaction
+	@param {Array} channels
+*/
+export const insertChannels = async (db: DB, channels: Channel[]) => {
+	let list = await db.execO<Channel>(`SELECT * FROM Channels`)
+	const stmt = await db.prepare(
+		`INSERT INTO Channels (slug, title, created_at, status, author_slug, flags) VALUES (?, ?, ?, ?, ?, ?);`,
 	);
-	query.forEach((q) => {
-		if (!list.has(q[key])) list.set(q[key], q);
+
+	channels.forEach(async (v) => {
+		// if (this.#list.has(v[keys.Channels])) return;
+		await stmt.run(
+			db,
+			v.slug,
+			v.title,
+			v.created_at,
+			v.status,
+			v.author_slug,
+			v.flags,
+		);
 	});
-}
 
-export const insertChannels = (channel: Channel[]) => {};
+	stmt.finalize(null);
+};
 
-export const insertBlocks = (blocks: Block[]) => {};
+/**
+	Insert an array off items into the given database.
+	Expects the table to already exist
+	@param {DB} db - DB Connection. Recommended to run inside a transaction
+	@param {Array} blocks
+*/
+export const insertBlocks = async (db: DB, blocks: Exclude<Block, "id">[]) => {
+	let list = await db.execO<Block>(`SELECT * FROM Blocks`);
 
-class Channels {
-	#list = $state<Map<Channel["slug"], Channel>>(new SvelteMap());
+	const stmt = await db.prepare(`
+		INSERT INTO Blocks
+						(title,filename,description,type,content,image,created_at,updated_at,source)
+		VALUES 	(		 ?, 			?, 				  ?,   ?,      ?,    ?,         ?,         ?,     ?);
+		`);
 
-	get list() {
-		return this.#list;
-	}
-
-	init(db: DB) {
-		// load existing items from memory into store
-		db.execO<Channel>(`SELECT * FROM Channels`).then((q) => {
-			this.#list = q.reduce(
-				(acc, curr) => acc.set(curr.slug, curr),
-				this.#list,
-			);
-		});
-
-		// callback to upload store on db change
-		// type === 18 is an addition have not worked on deletions or updates yet
-		db.onUpdate((type, _, table, row) => {
-			if (table !== "Channels") return;
-			console.log(`row ${row} in ${table} was ${type}`);
-			pull({ db, row, list: this.#list, key: keys.Channels, table });
-			// this.pull(db, row)
-		});
-	}
-
-	async push(db: DB, channels: Channel[]) {
-		const stmt = await db.prepare(
-			`INSERT INTO Channels (slug, title, created_at, status, author_slug, flags) VALUES (?, ?, ?, ?, ?, ?);`,
+	blocks.forEach(async (v) => {
+		// if (.has(v[keys.Blocks])) return;
+		await stmt.run(
+			db,
+			v.title,
+			v.filename,
+			v.description,
+			v.type,
+			v.content,
+			v.image,
+			v.created_at,
+			v.updated_at,
+			v.source,
 		);
-		await db.tx(async (tx) => {
-			channels.forEach(async (v) => {
-				if (this.#list.has(v[keys.Channels])) return;
-				await stmt.run(
-					tx,
-					v.slug,
-					v.title,
-					v.created_at,
-					v.status,
-					v.author_slug,
-					v.flags,
-				);
-			});
-		});
-
-		stmt.finalize(null);
-		// this.pull(promiser)
-		// promiser('close', { dbId })
-		// console.log(this.#list)
-	}
-}
-
-export const channels = new Channels();
-
-class Blocks {
-	init(db: DB) {
-		db.execO<Block>(`SELECT * FROM Blocks`).then((q) => {
-			this.#list = q.reduce((acc, curr) => acc.set(curr.id, curr), this.#list);
-		});
-
-		db.onUpdate((type, _, table, row) => {
-			if (table !== "Blocks") return;
-			console.log(`row ${row} in ${table} was ${type}`);
-			pull({ db, row, list: this.#list, key: keys.Blocks, table });
-		});
-	}
-
-	async push(db: DB, blocks: Exclude<Block, "id">[]) {
-		const stmt = await db.prepare(
-			`INSERT INTO Blocks (title,filename,description,type,content,image,created_at,updated_at,source) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`,
-		);
-
-		await db.tx(async (tx) => {
-			blocks.forEach(async (v) => {
-				if (this.#list.has(v[keys.Blocks])) return;
-				await stmt.run(
-					tx,
-					v.title,
-					v.filename,
-					v.description,
-					v.type,
-					v.content,
-					v.image,
-					v.created_at,
-					v.updated_at,
-					v.source,
-				);
-			});
-		});
-		stmt.finalize(null);
-		// console.log(this.list)
-	}
-}
+	});
+	stmt.finalize(null);
+};
