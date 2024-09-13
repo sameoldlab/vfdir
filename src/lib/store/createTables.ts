45a12e4f6612f07b2562c508d9cba3c0a3de1959
ibro <sameoldlab@proton.me>
2024-09-13 16:27:43 +0100

    move initTables to separate file

---
+++ src/lib/store/createTables.ts
@@ -0,0 +1,22 @@
+import Stmt from "$lib/store/createTables.sql?raw"
+import type { DB } from "@vlcn.io/crsqlite-wasm"
+
+export async function createTables(db: DB) {
+	try {
+		const isReady = await db.execA(`SELECT name FROM sqlite_master WHERE type='table' AND name='Users';`)
+		if (isReady.length > 0) return
+
+		console.debug('Initializing database...')
+		await db.tx(tx => tx.exec(Stmt))
+
+		// user_id INT NOT NULL default 0,
+		// updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		// FOREIGN KEY (user_id) REFERENCES Users(rowid)
+		// kind = 'default | 'profile'
+		// Tags: published, open, collaboration, (kind == 'default' ? null : 'profile')
+
+
+	} catch (err) {
+		console.error('CAUGHT', err.name, err.message)
+	}
+}
--- src/lib/store/sqlite.svelte.ts
+++ src/lib/store/sqlite.svelte.ts
@@ -21,7 +21,6 @@ function init() {
 		try {
 			const sqlite = await initWasm(() => wasmUrl);
 			cr.db = await sqlite.open("my-database.db");
-			await initDb(cr.db)
 			cr.fulfilled = cr.db !== undefined
 		} catch (err) {
 			console.error(err)
@@ -32,78 +31,4 @@ function init() {
 	if (cr.db == undefined) initialize()
 
 	return cr
-}
-
-async function initDb(db: DB) {
-	try {
-		const isReady = await db.execA(`SELECT name FROM sqlite_master WHERE type='table' AND name='Users';`)
-		if (isReady.length > 0) return
-
-		console.debug('Initializing database...')
-		await db.tx(tx => tx.exec(`
-			pragma journal_mode = wal;
-			CREATE TABLE IF NOT EXISTS Users(
-				id TEXT PRIMARY KEY NOT NULL,
-				slug TEXT,
-				firstname TEXT,
-				lastname TEXT,
-				avatar TEXT
-			);
-
-			CREATE TABLE IF NOT EXISTS Channels(
-				id TEXT PRIMARY KEY NOT NULL,
-				slug TEXT,
-				title TEXT DEFAULT '',
-				created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-				status TEXT DEFAULT 'private',
-				author_slug TEXT DEFAULT 'local',
-				flags TEXT default '[]',
-				arena_id INT
-			);
-
-			INSERT INTO Users(id) VALUES ('local');
-			
-			CREATE TABLE IF NOT EXISTS Blocks(
-				id TEXT PRIMARY KEY NOT NULL,
-				title TEXT DEFAULT '',
-				type TEXT,
-				created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-				updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
-				description TEXT DEFAULT '',
-				content TEXT,
-				image TEXT,
-				source TEXT,
-				filename TEXT,
-				author_id TEXT DEFAULT 'local',
-				arena_id INTEGER
-			);
-
-			CREATE TABLE IF NOT EXISTS Providers(
-				id TEXT PRIMARY KEY NOT NULL,
-				url TEXT,
-				name TEXT
-			);
-
-			CREATE TABLE IF NOT EXISTS Connections(
-				block_id INTEGER NOT NULL,
-				channel_slug TEXT NOT NULL,
-				is_channel INTEGER DEFAULT 0,
-				position INTEGER,
-				selected INTEGER,
-				connected_at TEXT,
-				user_id INTEGER
-			);
-		`
-		))
-
-		// user_id INT NOT NULL default 0,
-		// updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		// FOREIGN KEY (user_id) REFERENCES Users(rowid)
-		// kind = 'default | 'profile'
-		// Tags: published, open, collaboration, (kind == 'default' ? null : 'profile')
-
-
-	} catch (err) {
-		console.error('CAUGHT', err.name, err.message)
-	}
 }
