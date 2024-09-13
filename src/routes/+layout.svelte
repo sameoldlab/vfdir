5e3f4b984d7335bccdedb5757a2b1175af1a2042
ibro <sameoldlab@proton.me>
2024-09-13 16:18:20 +0100

    update layout to new system

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,38 @@
-<script>
+<script lang='ts'>
   import '../app.css'
   import Header from '$lib/components/header.svelte'
-	import { db } from '$lib/store/sqlite.svelte'
+	import { initTables } from '$lib/store/sqlite.svelte'
+	import { pool } from '$lib/store/connectionPool.svelte'
+  import { untrack } from 'svelte'
 	import { channels } from '$lib/store/data.svelte'
 	import { bootstrap } from '$lib/store/sync.svelte'
 	let { children } = $props();
 
 	let ready = $state(false)
+	let error = $derived(pool.status && pool.status !== 'available' ? pool.status : undefined)
+	$effect.pre(() => console.log('status: ', pool.status))
+
 	$effect(()=> {
-		if(db.fulfilled) {
-			channels.init(db.db)
-			ready = true
-			bootstrap(db.db)
+		if(pool.status === 'available') {
+			untrack(() => {
+				pool.exec(async (db) => {
+					await initTables(db)
+					// channels.init(db)
+					bootstrap(db)
+				})
+			})
 		}
+			// ready = true
 	})
 
 </script>
 
 <Header />
 
-{#if !ready }
+{#if error}
+	Error initializing database
+	<pre><code>{error}</code></pre>
+{:else if !ready }
 	setting up sqlite
 {:else}
 	{@render children()}
