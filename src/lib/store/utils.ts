24412884a522ed8742d275c4c22f388811d5fe42
ibro <sameoldlab@proton.me>
2024-09-13 16:15:11 +0100

    move types

--- src/lib/store/utils.ts
+++ src/lib/store/utils.ts
@@ -1,33 +1,5 @@
 import type { DB } from "@vlcn.io/crsqlite-wasm";
-
-type ChannelStatus = "private" | "closed" | "public";
-type ChannelFlags = "published" | "collaboration" | "default" | "profile";
-
-export type Block = {
-	id: number;
-	title: string;
-	filename?: string;
-	description: string;
-	type: string;
-	content?: string;
-	image?: string;
-	created_at: string;
-	updated_at: string;
-	source: string;
-	author_id: string;
-	arena_id: number;
-};
-
-export type Channel = {
-	slug: string;
-	title: string;
-	updated: Date;
-	created_at: Date;
-	status: ChannelStatus;
-	author_slug: string;
-	flags: ChannelFlags[];
-	blocks: Block[];
-};
+import type { Block, Channel } from "$lib/types";
 
 /**
 	Insert an array off items into the given database.
