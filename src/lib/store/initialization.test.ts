94c6faf1c40a10a1b911a9a78738a12e69095bc6
ibro <sameoldlab@proton.me>
2024-09-17 20:18:39 +0100

    merge bootstrap tests

--- src/lib/store/initialization.test.ts
+++ src/lib/store/initialization.test.ts
@@ -5,6 +5,7 @@ import { assert, is, StructError, validate } from 'superstruct';
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { createTables } from "./createTables";
 import { Block, Channel, Connections, Providers, Users } from './schema';
+import { bootstrap } from "./sync.svelte";
 
 describe('Database initialization', () => {
   let db: DB
@@ -18,14 +19,35 @@ describe('Database initialization', () => {
     expect(() => createTables(db)).not.toThrow()
   })
 
-  it('Users table matches type', async () => {
-    expect(() => assert(user, Users)).not.toThrowErrorMatchingSnapshot()
-    const user = (await db.execO('select * from users limit 1'))[0]
-  })
-
   it('has an initial user', async () => {
     await createTables(db)
-    expect(() => users.id).toEqual('local')
+    const user = (await db.execO('select * from users limit 1'))[0]
+    expect(user.id).toEqual('local')
+  })
+})
+
+
+describe('Bootstrap database and validate types', async () => {
+  const sqlite = await initWasm(() => wasmUrl)
+  const db = await sqlite.open(":memory:");
+  await createTables(db)
+
+  afterAll(() => { if (db) db.close() })
+
+  it('runs bootstrap without error', async () => {
+    await createTables(db)
+    expect(() => bootstrap(db)).not.toThrow()
+  })
+
+  // reinserting the same data does not duplicate rows
+  // adds in all blocks and connections
+  // can find a providers based on the block
+  // lists all blocks for a channel
+  // lists all channels for a block
+
+  it('Users table matches type', async () => {
+    const user = (await db.execO('SELECT * FROM Users  limit 1'))[0]
+    expect(() => assert(user, Users)).not.toThrow()
   })
 
   it('Blocks table matches type', async () => {
