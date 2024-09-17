ebca6212dc6ec5bab8fb9e8eb142acaa3e08e09b
ibro <sameoldlab@proton.me>
2024-09-17 16:14:17 +0100

    convert schema types to use

--- src/lib/store/schema.ts
+++ src/lib/store/schema.ts
@@ -3,33 +3,35 @@
 // FOREIGN KEY (user_id) REFERENCES Users(rowid)
 // kind = 'default | 'profile'
 // Tags: published, open, collaboration, (kind == 'default' ? null : 'profile')
+import {
+	array,
+	coerce,
+	date,
+	enums,
+	number,
+	object,
+	nullable,
+	string,
+	type Infer
+} from 'superstruct'
 
-interface BlockBase {
-    id: string;
-    title: string;
-    type: string;
-    description: string;
-    content?: string;
-    image?: string;
-    source?: string;
-    filename?: string;
-    provider_id: string;
-    arena_id?: number;
-    /** creator of block */
-    author_id: string;
-}
+const parseDate = coerce(string(), date(), (value) => new Date(value))
 
-// Block interface for use in application logic
-export interface Block extends BlockBase {
-    updated_at: Date;
-    created_at: Date;
-}
-
-// Raw block data as stored in the database
-export interface BlockRaw extends BlockBase {
-    updated_at: string;
-    created_at: string;
-}
+export const Block = object({
+	id: string(),
+	title: string(),
+	type: string(),
+	updated_at: string(),
+	created_at: string(),
+	description: string(),
+	content: nullable(string()),
+	image: nullable(string()),
+	source: nullable(string()),
+	filename: nullable(string()),
+	provider_id: nullable(string()),
+	arena_id: nullable(number()),
+	author_id: string()
+})
 
 const blocks = `
 CREATE TABLE IF NOT EXISTS Blocks(
@@ -46,41 +48,37 @@ CREATE TABLE IF NOT EXISTS Blocks(
 	provider_id TEXT,
 	author_id TEXT DEFAULT 'local',
 	arena_id INTEGER,
-	foreign key (author_id) refrenences Users(id)
+	foreign key (author_id) references Users(id)
 );
 `
 
-export type ChannelStatus = 'private' | 'closed' | 'public'
-export type ChannelFlag = 'published' | 'collaboration' | 'default' | 'profile'
+// Application Block Schema
+const BlockParsed = object({
+	...Block.schema,
+	updated_at: date(),
+	created_at: date()
+})
 
-// Base interface with common properties
-interface ChannelBase {
-    slug?: string
-    title: string
-    status: ChannelStatus
-    author_id: string
-    /** if imported from external service,
-     * service identifier ':' imported id
-     * @example `arena:2948201`
-     */
-    external_id?: string
-}
+export type Block = Infer<typeof Block>
+export type BlockParsed = Infer<typeof BlockParsed>
 
-// Channel interface for use in application logic
-export interface Channel extends ChannelBase {
-    updated_at: Date
-    created_at: Date
-    flags: ChannelFlag[]
-}
-
-// Raw channel data as stored in the database
-export interface ChannelRaw extends ChannelBase {
-    id: string
-    updated_at: string
-    created_at: string
-    /** `JSON.stringified` array */
-    flags: string
-}
+// Channels
+export const Channel = object({
+	id: string(),
+	slug: nullable(string()),
+	title: string(),
+	/** `JSON.stringified` array */
+	flags: string(),
+	status: enums(['private', 'closed', 'public']),
+	updated_at: string(),
+	created_at: string(),
+	author_id: string(),
+	/** if imported from external service,
+	 * service identifier ':' imported id
+	 * @example `arena:2948201`
+	 */
+	external_id: nullable(string())
+})
 
 const channels = `
 CREATE TABLE IF NOT EXISTS Channels(
@@ -95,19 +93,28 @@ CREATE TABLE IF NOT EXISTS Channels(
 	external_id TEXT UNIQUE
 );
 `
-interface ConnectionsBase {
-	child_id: string
-	parent_id: string
-	/** if child block is of type Channel */
-	is_channel: 0 | 1
-	position?: number
-	selected: 0 | 1
-	connected_at: string
-	/** User who created the connection */
-	user_id: string
-}
+export const ChannelParsed = object({
+	...Channel.schema,
+	updated_at: date(),
+	created_at: date(),
+	flags: array(enums(['published', 'collaboration', 'default', 'profile']))
+})
 
-export interface ConnectionsRaw extends ConnectionsBase {}
+export type Channel = Infer<typeof Channel>
+export type ChannelParsed = Infer<typeof ChannelParsed>
+
+// Connections
+export const Connections = object({
+	child_id: string(),
+	parent_id: string(),
+	/** if child block is of type Channel */
+	is_channel: enums([0, 1]),
+	position: nullable(number()),
+	selected: enums([0, 1]),
+	connected_at: string(),
+	/** User who created the connection */
+	user_id: string(),
+})
 
 const connections = `
 CREATE TABLE IF NOT EXISTS Connections(
@@ -121,8 +128,18 @@ CREATE TABLE IF NOT EXISTS Connections(
 );
 `
 
-export const schema = /*sql*/ `
-pragma journal_mode = wal;
+export type Connections = Infer<typeof Connections>
+
+// Users
+export const Users = object({
+	id: string(),
+	slug: nullable(string()),
+	firstname: nullable(string()),
+	lastname: nullable(string()),
+	avatar: nullable(string()),
+})
+
+const users = `
 CREATE TABLE IF NOT EXISTS Users(
 	id TEXT PRIMARY KEY NOT NULL,
 	slug TEXT,
@@ -130,13 +147,39 @@ CREATE TABLE IF NOT EXISTS Users(
 	lastname TEXT,
 	avatar TEXT
 );
-INSERT INTO Users(id) VALUES ('local');
-${blocks}
-${channels}
+`
+export type Users = Infer<typeof Users>
+
+// Providers
+export const Providers = object({
+	id: string(),
+	url: nullable(string()),
+	name: nullable(string()),
+})
+const providers = `
 CREATE TABLE IF NOT EXISTS Providers(
 	id TEXT PRIMARY KEY NOT NULL,
 	url TEXT,
 	name TEXT
 );
+`
+export type Providers = Infer<typeof Providers>
+
+// schema
+export const schema = /*sql*/ `
+pragma journal_mode = wal;
+${users}
+INSERT INTO Users(id) VALUES ('local');
+${blocks}
+${channels}
 ${connections}
+${providers}
+
+CREATE INDEX idx_blocks_type_author_id ON Blocks(type, author_id);
+CREATE INDEX idx_blocks_author_id ON Blocks(author_id);
+CREATE INDEX idx_blocks_created_at ON Blocks(created_at);
+
+CREATE INDEX idx_connections_child_id ON Connections(child_id);
+CREATE INDEX idx_connections_parent_child ON Connections(parent_id, child_id);
+CREATE INDEX idx_connections_position ON Connections(parent_id, position);
 `
