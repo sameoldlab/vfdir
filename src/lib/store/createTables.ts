5d82d8fa744563da353869168d25120b8c9a9b66
ibro <sameoldlab@proton.me>
2024-09-17 16:55:18 +0100

    remove error handling from createTables

--- src/lib/store/createTables.ts
+++ src/lib/store/createTables.ts
@@ -2,21 +2,17 @@ import { schema } from '$lib/store/schema'
 import type { DB } from "@vlcn.io/crsqlite-wasm"
 
 export async function createTables(db: DB) {
-	try {
-		const isReady = await db.execA(`SELECT name FROM sqlite_master WHERE type='table' AND name='Users';`)
-		if (isReady.length > 0) return
+	const isReady = await db.execA(`SELECT name FROM sqlite_master WHERE type='table' AND name='Users';`)
+	if (isReady.length > 0) return
 
-		console.debug('Initializing database...')
-		await db.tx(tx => tx.exec(schema))
+	console.debug('Initializing database...')
+	await db.tx(tx => tx.exec(schema))
 
-		// user_id INT NOT NULL default 0,
-		// updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-		// FOREIGN KEY (user_id) REFERENCES Users(rowid)
-		// kind = 'default | 'profile'
-		// Tags: published, open, collaboration, (kind == 'default' ? null : 'profile')
+	// user_id INT NOT NULL default 0,
+	// updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	// FOREIGN KEY (user_id) REFERENCES Users(rowid)
+	// kind = 'default | 'profile'
+	// Tags: published, open, collaboration, (kind == 'default' ? null : 'profile')
 
 
-	} catch (err) {
-		console.error('CAUGHT', err.name, err.message)
-	}
 }
