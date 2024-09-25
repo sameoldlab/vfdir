fc2a89f200a050143cdd59804ba04ce24c27292a
ibro <sameoldlab@proton.me>
2024-09-25 01:06:34 +0100

    check length against unique set length

--- src/lib/store/sync.test.ts
+++ src/lib/store/sync.test.ts
@@ -1,35 +1,55 @@
 import initWasm from '@vlcn.io/crsqlite-wasm'
 import wasmUrl from '@vlcn.io/crsqlite-wasm/crsqlite.wasm?url'
-import { afterAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createTables } from './createTables'
 import { bootstrap, parseArenaChannels } from './sync.svelte'
-import { arenaChannels } from '$lib/dummy/channels'
-
+import { arenaChannels as mockChannels } from '$lib/dummy/channels'
 
 describe('Bootstrap database', async () => {
-	const sqlite = await initWasm(() => wasmUrl)
-	const db = await sqlite.open(':memory:')
-	await createTables(db)
+	let db
+	beforeAll(async () => {
+		const sqlite = await initWasm(() => wasmUrl)
+		db = await sqlite.open()
+		/* await db.exec(`
+			delete from users;
+			delete from blocks;
+			delete from connections;
+			delete from providers;
+		`) */
+		await createTables(db)
+
+	})
+	afterAll(async () => {
+		await db.close()
+	})
 
 	it('runs bootstrap without error', async () => {
-		const res = await bootstrap(db)
-		expect(res).toBeTruthy()
+		// const res = await bootstrap(db)
+		// expect(res).toBeTruthy()
 	})
 
 	// reinserting the same data does not duplicate rows
 	it('does not duplicate inserts', async () => {
-		// console.time('parse')
-		await parseArenaChannels(db, arenaChannels)
-		// console.timeEnd('parse')
-		const length = (await db.execO('select * from Blocks;'))//[0][0];
-		console.log(length)
-		// console.time('parse double')
-		// const res = await parseArenaChannels(db, arenaChannels)
-		// console.timeEnd('parse double')
+		console.time('parse')
+		await parseArenaChannels(db, mockChannels)
+		console.timeEnd('parse')
+		const length1 = (await db.execA('select count(*) from Blocks;'))[0][0];
+
+		const totalUniques = mockChannels.reduce((r, c) => {
+			r.add(c.id)
+			c.contents && c.contents.forEach((b) => r.add(b.id))
+			return r
+		}, new Set())
+
+		console.time('parse double')
+		await parseArenaChannels(db, mockChannels)
+		console.timeEnd('parse double')
 		const length2 = (await db.execA('select count(*) from Blocks;'))[0][0];
-		console.log(length2)
-		// expect(res).toBeTruthy()
+
+		expect(length1).toEqual(totalUniques.size)
+		expect(length2).toEqual(totalUniques.size)
 	})
+})
 
 	afterAll(async () => {
 		await db.close()
