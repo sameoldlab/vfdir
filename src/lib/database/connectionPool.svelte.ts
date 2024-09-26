c1a96b1bdd2ca97d1cddde0da420bf43502aa565
ibro <sameoldlab@proton.me>
2024-09-26 08:05:45 +0100

    formatting

--- src/lib/database/connectionPool.svelte.ts
+++ src/lib/database/connectionPool.svelte.ts
@@ -1,22 +1,21 @@
-import initWasm, { DB, SQLite3 } from "@vlcn.io/crsqlite-wasm";
-import wasmUrl from "@vlcn.io/crsqlite-wasm/crsqlite.wasm?url";
-import { SvelteSet } from "svelte/reactivity";
+import initWasm, { DB, SQLite3 } from '@vlcn.io/crsqlite-wasm'
+import wasmUrl from '@vlcn.io/crsqlite-wasm/crsqlite.wasm?url'
+import { SvelteSet } from 'svelte/reactivity'
 
-type DELETE = 9;
-type INSERT = 18;
-type UPDATE = 23;
-type UpdateType = DELETE | INSERT | UPDATE;
-type UpdateEvent = [type: UpdateType, db: string, table: string, rowid: bigint];
+type DELETE = 9
+type INSERT = 18
+type UPDATE = 23
+type UpdateType = DELETE | INSERT | UPDATE
+type UpdateEvent = [type: UpdateType, db: string, table: string, rowid: bigint]
 
 export class DbPool {
-	#maxConnections: number;
-	#connections = new SvelteSet<DB>();
-	dbName: string;
-	status = $state<'available'>();
-	#queries = [];
+	#maxConnections: number
+	#connections = new SvelteSet<DB>()
 	#sqlite: SQLite3
+	dbName: string
 	status = $state<'available' | 'loading' | 'error'>('loading')
 	error = $state()
+	#queries = []
 
 	constructor(
 		{ maxConnections, dbName } = { maxConnections: 5, dbName: "vfdir.db" },
@@ -29,19 +28,23 @@ export class DbPool {
 		this.#sqlite = this.#sqlite || await this.#initSql()
 
 		if (this.#connections.size < this.#maxConnections) {
-			let connection: DB | undefined;
+			let connection: DB | undefined
 			try {
-				connection = await this.#sqlite.open(this.dbName);
+				connection = await this.#sqlite.open(this.dbName)
 			} catch (err) {
-				console.error(err);
+				console.error(err)
 			}
 			connection.onUpdate((...args) => {
 				this.#subscribe(...args);
 			});
 			this.#connections.add(connection);
 			return connection;
+				this.#subscribe(...args)
+			})
+			this.#connections.add(connection)
+			return connection
 		}
-		return [...this.#connections.values()][0];
+		return [...this.#connections.values()][0]
 	}
 	async #initSql() {
 		try {
@@ -54,34 +57,34 @@ export class DbPool {
 		}
 	}
 	#subscribe(...[type, db, table, rowid]: UpdateEvent) {
-		console.log(this.#queries);
+		console.log(this.#queries)
 		switch (type) {
 			case 18: {
-				console.log(`Row ${rowid} inserted in ${db}:${table}`);
-				break;
+				console.log(`Row ${rowid} inserted in ${db}:${table}`)
+				break
 			}
 			case 9: {
-				console.log(`Row ${rowid} deleted in ${db}:${table}`);
-				break;
+				console.log(`Row ${rowid} deleted in ${db}:${table}`)
+				break
 			}
 			case 23: {
-				console.log(`Row ${rowid} updated in ${db}:${table}`);
-				break;
+				console.log(`Row ${rowid} updated in ${db}:${table}`)
+				break
 			}
 		}
 	}
 
 	async #close(connection: DB) {
-		const res = await connection.close();
-		this.#connections.delete(connection);
-		return res;
+		const res = await connection.close()
+		this.#connections.delete(connection)
+		return res
 	}
 
 	async #closeAll() {
 		for (const connection of this.#connections) {
-			await connection.close();
+			await connection.close()
 		}
-		this.#connections.clear();
+		this.#connections.clear()
 	}
 
 	query<R>(fn: (d: DB) => R) {
@@ -116,5 +119,5 @@ export class DbPool {
 	}
 }
 
-export const pool = new DbPool();
-// const res = dp.exec((x) => x.execO<{ id: string }>(""));
+export const pool = new DbPool()
+// const res = dp.exec((x) => x.execO<{ id: string }>(''))
