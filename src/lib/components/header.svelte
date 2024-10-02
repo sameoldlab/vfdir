2c98d7ab73fcc3a3e65e7cccc17e14077adeddeb
ibro <sameoldlab@proton.me>
2024-10-02 14:45:09 +0100

    update styles

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,17 +3,19 @@
     <a href="/">vfdir</a>
   </div>
   <nav>
-      <span>Search</span>
-      <span>About</span>
-      <span>Account</span>
+      <span > Search 	</span>
+      <span > About 		</span>
+      <a href='/local' > Account 	</a>
   </nav>
 </header>
 
 <style>
   header {
     display: flex;
-    /* position: sticky; */
+    position: sticky;
     top:0;
+    left: 0;
+    right: 0;
     padding-block: .25em;
     justify-content: space-between;
     padding-inline: 2em;
@@ -22,6 +24,7 @@
 		border-bottom: 1px solid var(--line);
 		font-size: .9rem;
 		font-weight: 600;
+		max-width: 100svw;
   }
 
   nav {
