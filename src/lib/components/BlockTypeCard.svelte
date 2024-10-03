c1db912ef9a08919faae1edc6b00ab828fc527d2
ibro <sameoldlab@proton.me>
2024-10-03 20:18:39 +0100

    basic grid view and block.type card

---
+++ src/lib/components/BlockTypeCard.svelte
@@ -0,0 +1,38 @@
+<script>
+	let { ...c } = $props()
+</script>
+
+<div class="item {status}">
+	<a href={c.type === 'channel' ? `${c.user}/${c.slug}` : `/block/${c.id}`}>
+		<div>
+			<p>
+				{c.title}
+			</p>
+		</div>
+	</a>
+</div>
+
+<style>
+	.item {
+		border: 1px solid #666;
+		box-sizing: border-box;
+		padding: 0.25em;
+		width: 100%;
+		aspect-ratio: 1 / 1;
+
+		& > a {
+			display: block;
+			width: 100%;
+			height: 100%;
+		}
+	}
+	.public {
+		border: 1px solid green;
+	}
+	.private {
+		border: 1px solid #666;
+	}
+	.closed {
+		border: 1px solid red;
+	}
+</style>
