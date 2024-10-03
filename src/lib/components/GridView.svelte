f021414b16bc7d4839716845b6714869462a28f3
ibro <sameoldlab@proton.me>
2024-10-03 20:18:39 +0100

    basic grid view and block.type card

--- src/lib/components/GridView.svelte
+++ src/lib/components/GridView.svelte
@@ -1,53 +1,25 @@
 <script lang="ts">
-  let {content} = $props()
+	import BlockTypeCard from './BlockTypeCard.svelte'
+
+	let { content } = $props()
+	console.log(content)
 </script>
 
 <div class="grid">
-  {#each content as {status, slug, title}}
-    <div class="item {status}">
-      <a href={slug}>
-        <div>
-          <p>
-            {title}
-          </p>
-        </div>
-      </a>
-    </div>
-  {/each}
+	{#each content as c, i (c.id)}
+		<BlockTypeCard {...c} />
+	{/each}
 </div>
 
 <style>
-  .grid {
-    --size: 260px;
-    display: grid;
-    justify-content: center;
-    gap: 1em;
-    grid-template-columns: repeat(auto-fill, minmax(var(--size), 1fr));
-    box-sizing: border-box;
-    padding: 1em;
-    
-    & .item {
-      box-sizing: border-box;
-      padding: .25em;
-      height: var(--size);
-
-      & > a {
-        display: block;
-        width: 100%;
-        height: 100%;
-      }
-    }
-  }
-  div {
-    display: block;
-  }
-  .public {
-      border: 1px solid green;
-  }
-  .private {
-      border: 1px solid #666;
-  }
-  .closed {
-    border: 1px solid red;
-  }
+	.grid {
+		--size: 260px;
+		display: grid;
+		gap: 1em;
+		grid-template-columns: repeat(auto-fill, minmax(var(--size), 1fr));
+		box-sizing: border-box;
+	}
+	div {
+		display: block;
+	}
 </style>
