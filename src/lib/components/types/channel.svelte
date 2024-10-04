b9209e931b728780a4845f181c08c4f43b427848
ibro <sameoldlab@proton.me>
2024-10-04 22:03:02 +0100

    split types inot separate components

---
+++ src/lib/components/types/channel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { ...c } = $props()
+</script>
+
+<div class="channel">
+	{c.title}
+	<br />
+	by {c.author_id}
+</div>
---
+++ src/lib/components/types/index.ts
@@ -0,0 +1,8 @@
+import attachment from './attachment.svelte'
+import channel from './channel.svelte'
+import image from './image.svelte'
+import link from './link.svelte'
+import media from './media.svelte'
+import text from './text.svelte'
+
+export { attachment, channel, image, link, media, text }
