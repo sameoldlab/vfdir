8a6b9fea8050dfb9b1e2489bc39be92af62cb46f
ibro <sameoldlab@proton.me>
2024-10-05 11:28:15 +0100

    add source link

--- src/lib/components/BlockTypeCard.svelte
+++ src/lib/components/BlockTypeCard.svelte
@@ -5,6 +5,20 @@
 
 	let { ...c }: Block | Channel = $props()
 	const Type = T[c.type]
+	const connect = (e: MouseEvent) => {
+		e.preventDefault()
+		console.log(c)
+	}
+	const makeLink = () => {
+		let link = ''
+		switch (c.source) {
+			case 'arena':
+				link = `are.na/${c.author_id}/${c.slug}`
+				break
+		}
+		return 'https://' + link
+	}
+	const source = !c.source ? null : c.type !== 'channel' ? c.source : makeLink()
 </script>
 
 <div class="block-item {status}">
@@ -20,6 +34,9 @@
 				<button class="end">...</button>
 			</div>
 			<div class="end">
+				{#if source}
+					<a href={source} class="btn">Source</a>
+				{/if}
 				<button onclick={connect} class="btn">Connect</button>
 			</div>
 		</div>
