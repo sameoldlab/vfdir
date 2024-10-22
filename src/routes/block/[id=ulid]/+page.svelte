d352d588cd299c544359dad56a33e88298a03681
ibro <sameoldlab@proton.me>
2024-10-22 17:56:42 +0100

    update gridView import

--- src/routes/block/[id=ulid]/+page.svelte
+++ src/routes/block/[id=ulid]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import {page} from '$app/stores'
-    import GridView from "$lib/components/GridView.svelte";
-  const channel = $page.params.id
+	import { page } from '$app/stores'
+	import GridView from '$lib/components/views/GridView.svelte'
+	const channel = $page.params.id
 </script>
+
 {channel}
