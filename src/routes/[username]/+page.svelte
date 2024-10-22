54152654427c6a428fee08b4d7d72f108c2e5a46
ibro <sameoldlab@proton.me>
2024-10-22 17:56:42 +0100

    update gridView import

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-  import {page} from '$app/stores'
-  import GridView from "$lib/components/GridView.svelte";
-  // import { getContents as getArenaContents } from '$lib/store/arena-apiv2.svelte.js'
+	import { page } from '$app/stores'
+	import GridView from '$lib/components/views/GridView.svelte'
+	// import { getContents as getArenaContents } from '$lib/store/arena-apiv2.svelte.js'
 
-	const {username} = $page.params
-	console.log(username);
-	
+	const { username } = $page.params
+	console.log(username)
+
 	// getArenaContents(channel).then(data => {		console.log(data)	})
 </script>
+
 <h1>vfdir / {username}</h1>
-<br>
+<br />
 
 <h2>Channels</h2>
