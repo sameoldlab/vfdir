d71245ff40fccef388a68365b020ace41db27346
ibro <sameoldlab@proton.me>
2024-10-24 19:00:06 +0100

    add detials to block route

--- src/routes/block/[id=ulid]/+page.svelte
+++ src/routes/block/[id=ulid]/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import GridView from '$lib/components/views/GridView.svelte'
-	const channel = $page.params.id
+	import BlockDetails from '$lib/components/BlockDetails.svelte'
+	const { id } = $page.params
 </script>
 
-{channel}
+<div>
+	<BlockDetails {id} />
+</div>
+
+<style>
+	div {
+		max-width: 80ch;
+		margin-inline: auto;
+		padding: 2em;
+	}
+</style>
