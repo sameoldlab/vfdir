2755bdb870aaeca79f1b07061dcd93cbe3099f43
ibro <sameoldlab@proton.me>
2024-10-27 15:15:40 +0100

    formatting

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -4,85 +4,82 @@
 /// <reference lib="webworker" />
 
 const sw = self as unknown as ServiceWorkerGlobalScope
-import { build, files, version } from '$service-worker';
+import { build, files, version } from '$service-worker'
 // Create a unique cache name for this deployment
-const CACHE = `cache-${version}`;
-
+const CACHE = `cache-${version}`
 const ASSETS = [
   ...build, // the app itself
   ...files  // everything in `static`
-];
+]
 
 // Initialization
-sw.addEventListener('install', (event) => {
+sw.oninstall = (event) => {
   // Create a new cache and add all files to it
   async function addFilesToCache() {
     console.log({ msg: 'adding files to cache', CACHE, ASSETS })
-    const cache = await caches.open(CACHE);
-    await cache.addAll(ASSETS);
+    const cache = await caches.open(CACHE)
+    await cache.addAll(ASSETS)
   }
 
-  event.waitUntil(addFilesToCache());
-});
+  event.waitUntil(addFilesToCache())
+}
 
-sw.addEventListener('activate', (event) => {
+sw.onactivate = (event) => {
   // Remove previous cached data from disk
   async function deleteOldCaches() {
     console.log({ msg: 'deleting old cache', caches })
     for (const key of await caches.keys()) {
-      if (key !== CACHE) await caches.delete(key);
+      if (key !== CACHE) await caches.delete(key)
     }
   }
 
-  event.waitUntil(deleteOldCaches());
-});
+  event.waitUntil(deleteOldCaches())
+}
 
 // Intercept requests 
-sw.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
+sw.onfetch = (event) => {
 
   async function respond() {
     console.log({ msg: 'responding to fetch event', event })
-    const url = new URL(event.request.url);
-    const cache = await caches.open(CACHE);
+    const url = new URL(event.request.url)
+    const cache = await caches.open(CACHE)
 
     // `build`/`files` can always be served from the cache
     if (ASSETS.includes(url.pathname)) {
-      const response = await cache.match(url.pathname);
+      const response = await cache.match(url.pathname)
 
       if (response) {
-        return response;
+        return response
       }
     }
 
     // for everything else, try the network first, but
     // fall back to the cache if we're offline
     try {
-      const response = await fetch(event.request);
-
+      const response = await fetch(event.request)
       // if we're offline, fetch can return a value that is not a Response
       // instead of throwing - and we can't pass this non-Response to respondWith
       if (!(response instanceof Response)) {
-        throw new Error('invalid response from fetch');
+        throw new Error('invalid response from fetch')
       }
 
       if (response.status === 200) {
         cache.put(event.request, response.clone());
       }
 
-      return response;
+      return response
     } catch (err) {
-      const response = await cache.match(event.request);
+      const response = await cache.match(event.request)
 
       if (response) {
-        return response;
+        return response
       }
 
       // if there's no cache, then just error out
       // as there is nothing we can do to respond to this request
-      throw err;
+      throw err
     }
   }
 
-  event.respondWith(respond());
-});
+  event.respondWith(respond())
+}
