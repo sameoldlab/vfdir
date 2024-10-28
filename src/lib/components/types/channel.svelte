824b5998087359eee12f3dbdede17ac01f3ed431
ibro <sameoldlab@proton.me>
2024-10-28 22:19:57 +0100

    fix author slug instead of id in channel details

--- src/lib/components/types/channel.svelte
+++ src/lib/components/types/channel.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	let { ...c } = $props()
+	import { pool } from '$lib/database/connectionPool.svelte'
+	import type { User } from '$lib/database/schema'
+	import { firstPick } from '@vlcn.io/xplat-api'
+	const author_id = pool.query<User, User['slug']>(
+		`select slug from Users where id = ? limit 1`,
+		[c.author_id],
+		firstPick
+	)
 </script>
 
 <div class="channel">
 	<p class="title">{c.title}</p>
-	<p class="author">by {c.author_id}</p>
+	<p class="author">by {author_id.data}</p>
 </div>
 
 <style>
