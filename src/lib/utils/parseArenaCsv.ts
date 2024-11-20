3330301eb8f78410ba340d4b998fc07dc104116b
ibro <sameoldlab@proton.me>
2024-11-20 06:06:08 +0100

    lowercase values

--- src/lib/utils/parseArenaCsv.ts
+++ src/lib/utils/parseArenaCsv.ts
@@ -14,28 +14,28 @@ import { parse } from 'csv-parse/browser/esm/sync'
 const date_int = coerce(number(), union([string(), date()]), (value) => typeof value === 'string' ? new Date(value).valueOf() : value.valueOf())
 // Define the structure for each item
 const Channel = object({
-  ID: string(),
-  Filename: string(),
-  Title: string(),
-  Description: string(),
-  'Created At': date_int,
-  'Updated At': date_int,
-  Source: string(),
+  id: coerce(number(), string(), (v) => parseInt(v)),
+  filename: string(),
+  title: string(),
+  description: string(),
+  created_at: date_int,
+  updated_at: date_int,
+  source: optional(string()),
 });
 
 type Channel = Infer<typeof Channel>;
 
 export function arenaCsvToObj(csv: string): Channel[] {
-  const lines = csv.split('\n');
-  const headers = lines[0].split(',');
   const res = parse(csv, {
     delimiter: ',',
     columns: true,
   })
   return res
+  let [h, ...rows] = csv.split('\n')
+  const headers = h.toLowerCase().replaceAll(' ', '_').split(',')
 
   // Validate headers
-  if (headers.join(',') !== 'ID,Filename,Title,Description,Created At,Updated At,Source') {
+  if (headers.join(',') !== 'id,filename,title,description,created_at,updated_at,source') {
     throw new Error('Invalid CSV headers');
   }
 
