81bfe7e7b2cfdf719315dfb210060d2218ed4f34
ibro <sameoldlab@proton.me>
2024-11-20 06:14:17 +0100

    record and watch eventStore functions

--- src/lib/database/events.ts
+++ src/lib/database/events.ts
@@ -2,18 +2,29 @@ import type { DELETE, UPDATE, UpdateType } from "@vlcn.io/xplat-api"
 import { DbPool } from "./connectionPool.svelte"
 
 const EVENT_DB_NAME = 'log'
+const VERSION = 1
 let eventDb: DbPool
 if (!eventDb) {
   console.warn('new event db')
 
   eventDb = new DbPool({ maxConnections: 1, dbName: `${EVENT_DB_NAME}.db` })
 }
+
+const now = () => new Date().valueOf()
+let deviceId: string = null
+
+export const record = async (
+  { originId, data, objectId, type }:
+    Pick<EventSchema, 'objectId' | 'data' | 'originId' | 'type'>
+) => {
+  if (deviceId === null) throw new Error('initialize deviceID before storing events')
+
   await eventDb.exec(async (db) => {
-    await db.exec(`insert into ${EVENT_DB_NAME} values(?,?,?,?)`, [JSON.stringify(data)])
+    await db.exec(`insert into ${EVENT_DB_NAME} (version, localId, originId, data, type, objectId) values(?,?,?,?,?,?)`, [VERSION, `${now()}:${deviceId}`, originId, JSON.stringify(data), type, objectId])
   })
 }
 
-export const watch = async () => eventDb.query(`select *,rowid from ${EVENT_DB_NAME}  order by rowid`)
+export const watchEvents = () => eventDb.query<EventSchema[]>(`select *,rowid from ${EVENT_DB_NAME} order by rowid`)
 
 type EventData = {
   version: number
