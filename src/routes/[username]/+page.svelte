b165a1e0697fd6bb198d3702cc6d3de6d79549bc
ibro <sameoldlab@proton.me>
2024-11-22 13:36:14 +0100

    display user blocks

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -2,14 +2,35 @@
 	import { page } from '$app/stores'
 	import View from '$lib/components/view.svelte'
 	import { pool } from '$lib/database/connectionPool.svelte'
+	import type { User } from '$lib/database/schema'
+	import { getChannels } from '$lib/services/api/arenav2'
+	import { first } from '$lib/utils/queryProcess'
 
 	const { username } = $derived($page.params)
+	const user = $derived(
+		pool.query<User, User>(
+			`select id from users where slug = ? limit 1`,
+			[username],
+			first
+		)
+	)
 	const channels = $derived(
 		pool.query(`select * from blocks where author_slug = ?`, [username])
 	)
-	$inspect(channels.data)
+	let error: string = $state(null)
+	$effect(() => {
+		if (user.loading) return
+		if (user.data === null)
+			error =
+				'this user has not been saved. Try going to one of their channels instead'
+		const [service, id] = user.data.id.split(':')
+		if (service === 'arena') getChannels(id)
+	})
 </script>
 
-{#if !channels.loading}
+{#if channels.error || error}
+	{error}
+	{channels.error}
+{:else if !channels.loading}
 	<View {...channels.data} />
 {/if}
