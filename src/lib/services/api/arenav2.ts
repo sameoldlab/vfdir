8437a631ea3acbf26e5e0d4f6a878400017e09cd
ibro <sameoldlab@proton.me>
2024-11-22 15:47:34 +0100

    better fetch error handling

--- src/lib/services/api/arenav2.ts
+++ src/lib/services/api/arenav2.ts
@@ -1,43 +1,68 @@
 import { type GetBlockApiResponse, type GetChannelsApiResponse, type GetUserChannelsApiResponse } from 'arena-ts'
-// const client = new ArenaClient({
-//   //   fetch: ("https://api.are.na/v2/",)
-// });
-// let Channels = [];
-// client.channels().then((val)=>{
-//     // Channels = val
-//     console.log(val)
-//   })
-
+import { pullArena } from '../sync.svelte'
+import { pool } from '$lib/database/connectionPool.svelte'
+interface FetchError extends Error {
+	status: number
+	statusText: string
+	url: string
+}
 export async function getChannels(id: number | string): Promise<GetUserChannelsApiResponse> {
 	let res = await fetch(`https://api.are.na/v2/users/${id}/channels?per=50`, {
 		headers: {
 			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${import.meta.env.VITE_ARENA_KEY}`,
 		},
 		method: 'GET',
 	})
-	const data = await res.json()
-	pool.exec(async (tx) => {
-		await pullArena(tx, ...data.channels)
-	})
-	return data
+	try {
+		if (!res.ok) {
+			const error: FetchError = new Error(`HTTP Error ${res.status}`)
+			error.status = res.status
+			error.statusText = res.statusText
+			error.url = res.url
+			throw error
+		}
+		const data = await res.json()
+		pool.exec(async (tx) => {
+			await pullArena(tx, ...data.channels)
+		})
+		return data
+
+	} catch (err) {
+		if (err instanceof TypeError) {
+			throw new Error('Type error: Fetch failed')
+			throw err
+		}
+	}
 }
 
 export async function getBlocks(channel: string): Promise<GetBlockApiResponse> {
-	console.log('get blocks: ', channel)
 	const res = await fetch(
 		`https://api.are.na/v2/channels/${channel}?per=100&sort=position&direction=asc`,
 		{
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': 'Bearer',
 			},
 			method: 'GET',
 		}
 	)
-	const data = await res.json()
-	pool.exec(async (tx) => {
-		await pullArena(tx, data)
-	})
-	return data
+	try {
+		if (!res.ok) {
+			const error: FetchError = new Error(`HTTP Error ${res.status}`)
+			error.status = res.status
+			error.statusText = res.statusText
+			error.url = res.url
+			throw error
+		}
+		const data = await res.json()
+		pool.exec(async (tx) => {
+			await pullArena(tx, data)
+		})
+		return data
+
+	} catch (err) {
+		if (err instanceof TypeError) {
+			throw new Error('Type error: Fetch failed')
+			throw err
+		}
+	}
 }
