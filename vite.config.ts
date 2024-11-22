010068f51e9c9c863908f707dd01765dd3a08e82
ibro <sameoldlab@proton.me>
2024-11-22 16:05:28 +0100

    use certs only in development

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { readFileSync } from 'fs'
+import { loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	server: {
 		port: 5461,
-		https: {
+		https: mode === 'development' ? {
 			key: readFileSync(`${__dirname}/certs/key.pem`),
 			cert: readFileSync(`${__dirname}/certs/cert.pem`)
-		},
+		} : null,
 		proxy: {}
 	},
 
@@ -23,4 +24,5 @@ export default defineConfig({
 			name: 'brave'
 		}
 	}
-})
+
+}))
