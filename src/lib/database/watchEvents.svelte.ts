2d51faa5bc39ed1755f0f89586ee84a294c12902
ibro <sameoldlab@proton.me>
2024-11-26 22:37:59 +0100

    populate objectpool from events

--- src/lib/database/watchEvents.svelte.ts
+++ src/lib/database/watchEvents.svelte.ts
@@ -1,9 +1,10 @@
 import { EventSchema, EventSchemaR } from "./schema"
-import { create, StructError } from "superstruct"
+import { create } from "superstruct"
 import { fromArenaBlock, fromArenaChannel, fromArenaUser, fromArenaConnection } from "$lib/services/fromArena"
 import type { StmtAsync, TXAsync } from "@vlcn.io/xplat-api"
 import { pool } from "./connectionPool.svelte"
 const channel = new BroadcastChannel('updates')
+import { Block, Channel, Connection, User } from "$lib/pools/block.svelte"
 
 export const watchEvents = () => {
   let lastRow = 0n
@@ -12,14 +13,13 @@ export const watchEvents = () => {
     if (ev.data) {
       const ub = [...ev.data.values()]
       pool.exec(async (tx) => {
-        await tx.execO<EventSchema>('select *,rowid from log where rowid between ? and ?', [ub[0], ub.at(-1)]).then(events => parseEvent(events, tx))
+        await tx.execO<EventSchema>('select *,rowid from log where rowid between ? and ?', [ub[0], ub.at(-1)]).then(events => parseEvent(events))
       })
     }
   })
 }
 
-async function parseEvent(events: EventSchema[], tx: TXAsync) {
-  const stmts = new Map<string, StmtAsync>()
+async function parseEvent(events: EventSchema[]) {
   for (const e of events) {
     let {
       type: [action, field],
@@ -32,38 +32,33 @@ async function parseEvent(events: EventSchema[], tx: TXAsync) {
       switch (field) {
         case 'block': {
           const obj = from_arena ? fromArenaBlock(data) : data
-          stmts.set(...(await insertO(tx, obj, 'blocks', stmts)))
+          new Block(obj)
           break;
         }
         case 'channel': {
           const obj = from_arena ? fromArenaChannel(data) : data
-          if ('length' in obj) {
-            console.warn({ device, from_arena, e, obj })
-          }
-          stmts.set(...(await insertO(tx, obj, 'blocks', stmts)))
+          new Channel(obj)
           break
         }
         case 'user': {
           const obj = from_arena ? fromArenaUser(data) : data
-          stmts.set(...(await insertO(tx, obj, 'users', stmts)))
+          new User(obj)
           break
         }
         case 'connection': {
           const obj = from_arena ? fromArenaConnection(data) : data
-          stmts.set(...(await insertO(tx, obj, 'connections', stmts)))
+          new Connection(obj)
           break
         }
       }
     } else if (action === 'mod') {
-
+      switch (field) {
+        case 'image': {
+          break;
+        }
+      }
     }
   }
-  return Promise.all(stmts).then(async stmts => {
-    for (const [_, stmt] of stmts) {
-      await stmt.finalize(tx)
-    }
-  })
-
 }
 
 async function insertO<O extends object>(tx: TXAsync, row: O, table: string, stmts: Map<string, StmtAsync>): Promise<[string, StmtAsync]> {
