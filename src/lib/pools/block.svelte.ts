7a6544cba76c0e0123aa6f95282a9cbfd559ed5b
ibro <sameoldlab@proton.me>
2024-11-28 23:35:28 +0100

    add keys to all classes, improve user data linking

--- src/lib/pools/block.svelte.ts
+++ src/lib/pools/block.svelte.ts
@@ -4,27 +4,31 @@ export const users = new Map<string, User>()
 export const media = new Map<string, string>()
 
 export class User {
-  id: string = $state()
+  key: string
+  id: string
   slug: string = $state()
   firstname: string = $state()
   lastname: string = $state()
   avatar: string = $state()
-  #channels: string[] = $state([])
-  #blocks: string[] = $state([])
+  #keys = new Set<string>()
+  #channels: string[] = []
+  #blocks: string[] = []
 
   constructor(obj: User) {
     this.id = `${obj.id}`
     this.slug = obj.slug
+    this.key = this.slug
     this.firstname = obj.firstname
     this.lastname = obj.lastname
     this.avatar = obj.avatar
-    users.set(this.slug, this)
+    users.set(this.key, this)
   }
-  addBlock(id: string) {
-    this.#blocks.push(`${id}`)
-  }
-  addChannel(slug: string) {
-    this.#channels.push(`${slug}`)
+  addEntry(key: string, type: 'blocks' | 'channels') {
+    if (this.#keys.has(key)) return
+    this.#keys.add(key)
+    if (type === 'channels')
+      this.#channels.push(key)
+    else this.#blocks.push(key)
   }
   get blocks() {
     return this.#blocks.map(id => blocks.get(id))
@@ -32,19 +36,28 @@ export class User {
   get channels() {
     return this.#channels.map(slug => channels.get(slug))
   }
+  get all(): (Channel | Block)[] {
+    const c = this.#channels.map(slug => channels.get(slug))
+    const b = this.#blocks.map(id => blocks.get(id))
+    return c.concat(b).sort((a, b) => a.updated_at - b.updated_at)
+  }
 }
+export type Child = Connection & ({ is_channel: false } & Block | { is_channel: true } & Channel)
 export class Connection {
-  id
-  parent_id
-  child_id
-  is_channel
-  position
-  selected
-  connected_at
-  user_slug
-  connected_by
+  key: string
+  id: string
+  parent_id: string
+  child_id: string
+  is_channel: true | false
+  position: number
+  selected: boolean
+  connected_at: number
+  user_slug: string
+  connected_by: string
+
   constructor(obj: Connection) {
     this.id = `${obj.id}`
+    this.key = this.id
     this.parent_id = `${obj.parent_id}`
     this.child_id = `${obj.child_id}`
     this.is_channel = obj.is_channel === 1
@@ -52,72 +65,84 @@ export class Connection {
     this.selected = obj.selected
     this.connected_at = obj.connected_at
     this.connected_by = obj.user_slug
-
-    if (this.is_channel) console.log(this)
   }
-  get() {
+  get(): Child {
     const child = this.is_channel ? channels.get(this.child_id) : blocks.get(this.child_id)
-    return child ? Object.assign(child, this) : this
+    if (!child) {
+      throw new Error(`Child not found for ${this.key} `);
+    }
+    return Object.assign(child, this) as Child
   }
 }
 export class Channel {
-  id
-  type
-  title
-  slug
-  description
-  created_at
-  updated_at
-  source
-  status
-  image
-  author_slug
-  #author_id
-  #blocks: Connection[] = $state([])
+  key: string
+  slug: string
+  id: string
+  title: string
+  description: string
+  created_at: number
+  updated_at: number
+  status: string
+  image: string
+  #author: string
+  get author() {
+    return users.get(this.#author)
+  }
+  #blocks: Connection[] = []
+  get blocks(): ((Block | Channel) & Connection)[] {
+    return this.#blocks.map(id => id.get())
+  }
   constructor(obj: Channel) {
     this.id = `${obj.id}`
-    this.type = obj.type
     this.title = obj.title
     this.slug = obj.slug
+    this.key = this.slug
     this.status = obj.status
     this.description = obj.description
     this.created_at = obj.created_at
     this.updated_at = obj.updated_at
     this.image = obj.image
-    this.source = obj.source
-    this.author_slug = obj.author_slug
-    channels.set(this.slug, this)
-  }
-  get author() {
-    return users.get(this.author_slug)
+    this.#author = obj.author_slug
+    channels.set(this.key, this)
+    populateUser(this.key, this.#author, 'channels')
   }
   addBlock(conn: Connection) {
     this.#blocks.push(new Connection(conn))
-  }
-  get blocks() {
-    return this.#blocks.map(id => id.get())
   }
   get length() {
     return this.#blocks.length
   }
 }
+function populateUser(key: string, userId: string, type = 'blocks') {
+  const user = users.get(userId)
+  if (user) user.addEntry(key, type)
+}
 export class Block {
-  id: string = $state('')
+  key: string
+  id: string
   title: string = $state('')
   description: string = $state('')
-  #connections: string[] = $state([])
   media?: string = $state('')
   content?: string = $state('')
-  #user: string = ''
   type
-  created_at
-  updated_at
+  created_at: number
+  updated_at: number
   filename
   provider_url
   image
   source
+  #author: string = ''
+  get author() {
+    const a = users.get(this.#author)
+    if (!a) throw Error(`${this.#author} not found`)
+    return a
+  }
+  #connections = new Set<string>()
+  get connections() {
+    return [...this.#connections.values()].map(c => channels.get(c))
+  }
 
-  constructor(b: BlockT) {
+  constructor(b: Block) {
     this.type = b.type
     this.created_at = b.created_at
     this.updated_at = b.updated_at
@@ -127,21 +152,17 @@ export class Block {
     this.source = b.source
 
     this.id = `${b.id}`
+    this.key = this.id
     this.title = b.title
     this.description = b.description
     this.media = b.media
     this.content = b.content
-    this.#user = b.author_slug
-    blocks.set(this.id, this)
+    this.#author = b.author_slug
+    blocks.set(this.key, this)
+
+    populateUser(this.key, this.#author)
   }
   addConnection(slug: string) {
-    this.#connections.push(slug)
-  }
-  get author() {
-    return users.get(this.#user)
-  }
-  get connections() {
-    const conns = this.#connections.map(c => channels.get(c))
-    return conns
+    this.#connections.add(slug)
   }
 }
