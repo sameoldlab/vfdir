8e1c1c922369614c3ebb8561b06df293d7247ebe
ibro <sameoldlab@proton.me>
2024-11-28 11:06:52 +0100

    migrate username/channels to object graph

--- src/routes/[username]/[channel]/+page.svelte
+++ src/routes/[username]/[channel]/+page.svelte
@@ -1,53 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 	import View from '$lib/components/view.svelte'
-	import { pool } from '$lib/database/connectionPool.svelte'
+	import { channels } from '$lib/pools/block.svelte'
 	import { getBlocks } from '$lib/services/api/arenav2'
-	import { first } from '$lib/utils/queryProcess'
 
-	const channel = $derived(
-		pool.query(
-			`
-		select 
-			b.id,b.slug,b.title,b.created_at,b.status,b.type,
-			coalesce(c.conn_count, 0) as size 
-		from blocks b
-		left join (
-			select parent_id,count(*) as conn_count
-			from connections
-			group by parent_id
-		) c on b.id = c.parent_id
-	  where slug=?
-	`,
-			[$page.params.channel],
-			first
-		)
-	)
-	$effect(() => {
-		getBlocks($page.params.channel)
-	})
-	const contents = $derived.by(() => {
-		if (!channel.loading && channel.data?.id) {
-			return pool.query(
-				`
-				SELECT b.*
-				FROM Connections conn
-				JOIN Blocks b ON conn.child_id = b.id
-				WHERE conn.parent_id = ?
-				ORDER BY conn.position asc;
-			`,
-				[channel.data.id]
-			)
-		} else {
-			return {
-				loading: true
-			}
-		}
-	})
+	const data = channels.get($page.params.channel).blocks
+
+	// $effect(() => {
+	// 	getBlocks($page.params.channel)
+	// })
 </script>
 
 <div>
-	{#if !channel.loading && channel.data === undefined}
+	{#if data.length === 0}
 		<div class="error">
 			There doesn't seem to be anything here. Searching arena for a matching
 			channel.
@@ -57,14 +22,7 @@
 				>
 			</p>
 		</div>
-	{:else if !contents.loading}
-		<View {...contents.data} />
+	{:else}
+		<View {data} />
 	{/if}
 </div>
-
-<style>
-	spacer {
-		height: 2rem;
-		display: block;
-	}
-</style>
