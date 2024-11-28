eb65cb0ddac6ffa047277ac714c2960c022f053a
ibro <sameoldlab@proton.me>
2024-11-28 11:06:52 +0100

    migrate username/channels to object graph

--- src/lib/components/views/MillerView.svelte
+++ src/lib/components/views/MillerView.svelte
@@ -1,79 +1,71 @@
 <script lang="ts">
-	import { Block, Channel, type BlocksRow } from '$lib/database/schema'
 	import { resizer, key } from '$lib/actions'
-	import { pushState } from '$app/navigation'
-	// import { page } from '$app/stores'
-	import { pool } from '$lib/database/connectionPool.svelte'
 	import BlockDetail from '$lib/components/BlockDetails.svelte'
 	import { getTree } from '$lib/stores.svelte'
-	import { first } from '$lib/utils/queryProcess'
+	import {
+		Block,
+		blocks,
+		Channel,
+		channels,
+		Connection
+	} from '$lib/pools/block.svelte'
 
-	let { ...data }: BlocksRow[] = $props()
+	let { data }: { data: (Connection & (Block | Channel))[] } = $props()
+	console.log(data)
 	const tree = getTree()
 
-	const addItem = (e: MouseEvent) => {
-		pushState('', {
-			show: 'add'
-		})
-	}
-
 	let previous = $derived.by(() => {
-		if ($tree.at(-1)?.route.id === '/')
-			return pool.query<BlocksRow>(
-				`select * from Blocks where type='channel' order by updated_at desc`
-			)
+		if ($tree.at(-1)?.route.id === '/') return // all channels
 
 		// if nested in a channel show the content of the parent channel
 		// select all blocks with a connection (child id to the given channel
 		if ($tree.at(-1) && 'channel' in $tree.at(-1)?.params)
-			return pool.query<BlocksRow>(
-				`
-				SELECT b.* from Blocks b 
-				JOIN Connections conn ON conn.child_id = b.id 
-				JOIN Blocks ch ON conn.parent_id = ch.id
-				WHERE ch.slug = ?
-				ORDER by conn.position DESC, conn.connected_at
-				`,
-				[$tree.at(-1).params.channel]
-			)
-
+			return channels.get($tree.at(-1).params.channel).blocks
 		return
 	})
 
-	/**
-	 * id of currently hovered item
-	 */
-	let col2Hover: string | undefined = $state(data[0]?.id)
+	/** key of currently hovered item */
+	let focused: Block['id'] | Channel['slug'] | undefined = $state()
+	let focused_is_channel = $state(false)
 </script>
 
 <main>
 	<div class="pane left">
-		{#if previous && previous.data.length > 1}
-			{#each previous.data as { id, user, slug, title }, i (id)}
-				<a tabindex="-1" href="{user}/{slug}" class="item">{title}</a>
+		{#if previous && previous.length > 1}
+			{#each previous as { id, author, title, ...p }, i (id)}
+				<a
+					tabindex="-1"
+					href={p.is_channel ? `/${author.id}/${p.child_id}` : `/block/${id}`}
+					class="item"
+				>
+					{title}
+				</a>
 			{/each}
 		{:else}
-			<div class="item">///</div>
+			<div class="item">//</div>
 		{/if}
 	</div>
 
 	<div class="handle" draggable use:resizer><div></div></div>
 
 	<div class="pane right">
-		<button class="item" use:key={{ right: 'add' }} onclick={addItem}
-			>+ Add Block</button
-		>
-		{#each data as { id, title, type, ...b }, i (id)}
+		{#each data as b, i (b.child_id)}
 			<a
-				onmouseover={() => (col2Hover = id)}
-				onfocus={() => (col2Hover = id)}
-				href={type === 'channel' ? `/${b.author_slug}/${b.slug}` : id}
+				onmouseover={() => {
+					focused_is_channel = b.is_channel
+					console.log(b)
+					focused = b.child_id
+				}}
+				onfocus={() => {
+					focused_is_channel = b.is_channel
+					console.log(focused_is_channel)
+					focused = b.child_id
+				}}
+				href={b.is_channel
+					? `/${b.author.slug}/${b.child_id}`
+					: `/block/${b.child_id}`}
 				class="item"
-				use:key>{title || '-'}</a
-			>
-		{:else}
-			<button class="item" use:key={{ left: 'back' }} onclick={addItem}
-				>..</button
+				use:key>{b.title || '-'}</a
 			>
 		{/each}
 	</div>
@@ -81,8 +73,19 @@
 	<div class="handle" draggable use:resizer><div></div></div>
 
 	<div class="pane detail">
-		{#if col2Hover}
-			<BlockDetail id={col2Hover} />
+		{focused}
+		{#if focused && !focused_is_channel}
+			<BlockDetail block={blocks.get(focused)} />
+		{:else if focused}
+			{#each channels.get(focused)?.blocks as b, i}
+				<a
+					href={b.is_channel
+						? `/${b.author.slug}/${b.child_id}`
+						: `block/${b.id}`}
+					class="item"
+					use:key>{b.title || '-'}</a
+				>
+			{/each}
 		{/if}
 	</div>
 </main>
@@ -166,11 +169,12 @@
 	.pane.right {
 		/* grid-column: 3 / 4; */
 		min-width: 10ch;
-		max-width: 30ch;
-		min-width: 1fr;
+		max-width: 50ch;
 		/* width: 40ch; */
 		/* background: brown; */
-		width: 1fr;
+		width: 2fr;
+		height: 100%;
+		overflow-x: scroll;
 	}
 	.pane.detail {
 		/* grid-column: 5 / full-end; */
