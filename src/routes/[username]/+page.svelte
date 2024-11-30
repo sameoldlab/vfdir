91004bbd95836fb869e4ec523adae4707a4bc80c
ibro <sameoldlab@proton.me>
2024-11-30 10:55:25 +0100

    migrate username route

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,36 +1,19 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 	import View from '$lib/components/view.svelte'
-	import { pool } from '$lib/database/connectionPool.svelte'
-	import type { User } from '$lib/database/schema'
-	import { getChannels } from '$lib/services/api/arenav2'
-	import { first } from '$lib/utils/queryProcess'
+	import { users } from '$lib/pools/block.svelte'
 
-	const { username } = $derived($page.params)
-	const user = $derived(
-		pool.query<User, User>(
-			`select id from users where slug = ? limit 1`,
-			[username],
-			first
-		)
-	)
-	const channels = $derived(
-		pool.query(`select * from blocks where author_slug = ?`, [username])
-	)
-	let error: string = $state(null)
-	$effect(() => {
-		if (user.loading) return
-		if (user.data === null)
-			error =
-				'this user has not been saved. Try going to one of their channels instead'
-		const [service, id] = user.data.id.split(':')
-		if (service === 'arena') getChannels(id)
-	})
+	const user = users.get($page.params.username)
+	const data = user?.all
 </script>
 
-{#if channels.error || error}
-	{error}
-	{channels.error}
-{:else if !channels.loading}
-	<View {...channels.data} />
+{#if !user}
+	<div class="error">
+		User: {$page.params.username} not found. Try searching one of their channels
+		instead.
+	</div>
+{:else if data.length === 0}
+	empty
+{:else}
+	<View {data} />
 {/if}
