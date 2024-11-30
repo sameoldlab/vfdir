bd2e720c42b452d5ea22baa1627a7aa8a2d9e737
ibro <sameoldlab@proton.me>
2024-11-30 19:22:29 +0100

    use object maps to dedupe new entries

--- src/lib/services/sync.svelte.ts
+++ src/lib/services/sync.svelte.ts
@@ -1,61 +1,41 @@
 import type { DB } from '@vlcn.io/crsqlite-wasm'
 import type { ArenaChannelWithDetails } from 'arena-ts'
-import { Block, Connection, type User } from '$lib/database/schema'
-import { arena_item_sync, arena_connection_import, arena_user_import, ev_stmt_close } from '$lib/database/events'
+import { arena_entry_sync, arena_connection_import, arena_user_import, ev_stmt_close } from '$lib/database/events'
 import type { TXAsync } from '@vlcn.io/xplat-api'
+import { blocks, users, channels } from '$lib/pools/block.svelte'
 
-export async function bootstrap(db: DB) {
-	// const arenaChannels = await getChannels()
-	// const arenaBlocks = await getBlocks()
-	// await parseArenaChannels(db, arenaChannels)
-	// const logs = watchEvents()
-
-	// await pullArena(db, ...arenaChannels)
-}
-
-
-
-export async function pullArena(db: DB | TXAsync, ...channels: ArenaChannelWithDetails[]) {
+export async function pullArena(db: DB | TXAsync, ...aChannels: ArenaChannelWithDetails[]) {
 	const dedupe = {
-		blocks: new Map<number, Block>(),
-		users: new Set<number>(),
-		conns: new Map<number, Set<number>>()
+		entries: new Map(blocks),
+		users: new Set<string>(users.keys()),
+		conns: channels.entries().reduce((acc, [k, v]) => {
+			if (!acc.has(k)) acc.set(k, new Set())
+			v.blocks.forEach(b => { acc.get(k).add(b.key) })
+			return acc
+		}, new Map<string, Set<string>>())
 	}
+
 
 	const promises: Promise<void>[] = []
 	const add = (p: Promise<void>) => promises.push(p)
-	await db.tx(async (db) => {
-		(await db.execO<Block>(`select * from Blocks`)).forEach((b) => {
-			dedupe.blocks.set(Number(b.id.split(':')[1]), b)
-		});
-		(await db.execA<User['id'][]>(`select id from Users`))
-			.forEach(u => {
-				dedupe.users.add(Number(u[0].split(':')[1]))
-			});
-		(await db.execA<Connection['id'][]>(`select id from Connections`)).forEach(i => {
-			const [_p, c] = i[0].split(':')
-			let p = Number(_p)
-			const set = dedupe.conns.get(p) ?? dedupe.conns.set(p, new Set()).get(p)
-			set.add(Number(c))
-		})
-	})
 
-	for (const chan of channels) {
-		let currentChan = dedupe.blocks.get(chan.id)
-		if (!currentChan) dedupe.blocks.set(chan.id, chan)
-		add(arena_item_sync(db, chan, currentChan))
+	for (const chan of aChannels) {
+		let currentChan = dedupe.entries.get(chan.slug)
+		if (!currentChan) dedupe.entries.set(chan.slug, chan)
+		add(arena_entry_sync(db, chan, currentChan))
 
 		if (!chan.contents) continue
 		for (const bl of chan.contents) {
-			const currentBlock = dedupe.blocks.get(bl.id)
-			if (!currentBlock) dedupe.blocks.set(bl.id, bl)
-			add(arena_item_sync(db, bl, currentBlock))
+			const key = bl.base_class === 'Channel' ? bl.slug : bl.id.toString()
+			const currentBlock = dedupe.entries.get(key)
+			if (!currentBlock) dedupe.entries.set(key, bl)
+			add(arena_entry_sync(db, bl, currentBlock))
 
-			if (!dedupe.users.has(bl.user.id)) {
+			if (!dedupe.users.has(bl.user.slug)) {
 				add(arena_user_import(db, bl.user))
-				dedupe.users.add(bl.user.id)
+				dedupe.users.add(bl.user.slug)
 			}
-			if (!dedupe.users.has(bl.connected_by_user_id)) {
+			if (!dedupe.users.has(bl.connected_by_user_slug)) {
 				const [first_name, last_name] = bl.connected_by_username.split(' ')
 				add(arena_user_import(db, {
 					id: bl.connected_by_user_id,
@@ -63,16 +43,18 @@ export async function pullArena(db: DB | TXAsync, ...channels: ArenaChannelWithD
 					first_name,
 					last_name,
 				}))
-				dedupe.users.add(bl.connected_by_user_id)
+				dedupe.users.add(bl.connected_by_user_slug)
 			}
 
-			const conn = dedupe.conns.get(chan.id)
+			const conn = dedupe.conns.get(chan.slug)
 			if (conn) {
-				if (!conn.has(bl.id)) {
-					add(arena_connection_import(db, chan, bl))
-					conn.add(bl.id)
-				}
-			} else dedupe.conns.set(chan.id, new Set())
+				if (conn.has(key)) continue
+				add(arena_connection_import(db, chan, bl))
+				conn.add(key)
+			} else {
+				add(arena_connection_import(db, chan, bl))
+				dedupe.conns.set(key, new Set())
+			}
 		}
 	}
 	await Promise.all(promises).then(() => {
